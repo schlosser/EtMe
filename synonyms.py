59c448234eb466dcad6a5db9c754049641fa37f4
jettca <jettca1@gmail.com>
2012-09-30 07:53:27 -0400

    removed print statement

--- synonyms.py
+++ synonyms.py
@@ -5,7 +5,6 @@ def getSynonyms(word):
     url = "http://words.bighugelabs.com/api/2/765022ea2b61ad9eb1abb967285eab2e/" + word + "/"
     synonyms = requests.get(url)
     synonyms = string.split(str(synonyms.text), "\n", 4) 
-    print(synonyms)
     newSynonyms = []
     for word in synonyms:
         words = string.split(word, "|")
