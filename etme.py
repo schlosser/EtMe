e0c46a77245414b38aa50f8012c84df712271a54
jettca <jettca1@gmail.com>
2012-09-30 02:00:57 -0400

    Created session for Foursquare access_token

--- etme.py
+++ etme.py
@@ -1,6 +1,6 @@
 # imports
-import sqlite3
-from flask import Flask, redirect, url_for, render_template, flash
+import getdata
+from flask import Flask, redirect, request, url_for, render_template, flash
 from foursquare import Foursquare
 
 app = Flask(__name__)
@@ -10,17 +10,20 @@ base_url = "http://localhost:5000"
 def login_page():
     return render_template("login.html")
 
-@app.route("/auth")
-def foursquare_connect():
-    # oauth
-    client = Foursquare(client_id = "3GZG0BE3SQSWRKGCXXSEJD4FM2TWGXTQOFQUWOKCQ3ZILXEP",
+def makeClient():
+    return Foursquare(client_id = "3GZG0BE3SQSWRKGCXXSEJD4FM2TWGXTQOFQUWOKCQ3ZILXEP",
             client_secret = "EXTDHANAPBAR3PDAGJU2DLLZFJPPH1KZYESMY00LXZCVTEBW",
             redirect_uri = base_url + "/auth2")
-    auth_uri = client.oauth.auth_url()
-    return redirect(auth_uri);
+
+@app.route("/auth")
+def foursquare_connect():
+    return redirect(makeClient().oauth.auth_url());
 
 @app.route("/auth2")
 def main_page():
+    client = makeClient()
+    access_token = client.oauth.get_token(request.args.getlist("code"))
+    session['access_token'] = access_token
     return render_template("main_page.html")
 
 app.debug = True
