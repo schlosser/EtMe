d17a151cf6907a789a4d07c5ec7561d82570c9cc
jettca <jettca1@gmail.com>
2012-09-30 02:25:14 -0400

    Fixed client token bug

--- etme.py
+++ etme.py
@@ -1,6 +1,6 @@
 # imports
 import getdata
-from flask import Flask, redirect, request, url_for, render_template, flash
+from flask import Flask, redirect, session, request, url_for, render_template, flash
 from foursquare import Foursquare
 
 app = Flask(__name__)
@@ -22,10 +22,12 @@ def foursquare_connect():
 @app.route("/auth2")
 def main_page():
     client = makeClient()
-    access_token = client.oauth.get_token(request.args.getlist("code"))
+    code = request.args.getlist("code")
+    access_token = client.oauth.get_token(str(code[0]))
     session['access_token'] = access_token
     return render_template("main_page.html")
 
 app.debug = True
+app.secret_key = "/x85/xfe/x98j/xc8FQb-/x88/xaf/x87/xda/xed/xba/n/x1dk/xbb//0b/xb06/xd2/x87"
 if __name__ == "__main__":
     app.run()
